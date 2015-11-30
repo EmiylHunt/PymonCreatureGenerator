ce9d45e2b6653be9f8c11b46332a4178e9bf02c4
Emily "Emiyl" Hunt <emiylhunt@gmail.com>
2015-11-29 23:59:41 -0500

    Created debug.py
    Some really boring tests.

---
+++ debug.py
@@ -0,0 +1,231 @@
+import PymonKit
+
+print('This is a test')
+print('If it asks you to input r-')
+print('-you should get an error message and then the prompt will repeat. You can enter a, b, c, or d to continue.')
+
+iprompt('Input lowercase a')
+print i
+print e
+i = 0
+e = 0
+
+iprompt('Input lowercase b')
+print i
+print e
+i = 0
+e = 0
+
+iprompt('Input lowercase c')
+print i
+print e
+i = 0
+e = 0
+
+iprompt('Input lowercase d')
+print i
+print e
+i = 0
+
+iprompt('Input lowercase r')
+i = 0
+e = 0
+
+eprompt('Input lowercase a')
+print e
+print i
+e = 0
+i = 0
+
+eprompt('Input lowercase b')
+print e
+print i
+e = 0
+i = 0
+
+eprompt('Input lowercase c')
+print e
+print i
+e = 0
+i = 0
+
+eprompt('Input lowercase d')
+print e
+print i
+e = 0
+i = 0
+
+eprompt('Input lowercase r')
+e = 0
+i = 0
+
+sprompt('Input lowercase a')
+print s
+print n
+s = 0
+n =0
+
+sprompt('Input lowercase b')
+print s
+print n
+s = 0
+n = 0
+
+sprompt('Input lowercase c')
+print s
+print n
+s = 0
+n = 0
+
+sprompt('Input lowercase d')
+print s
+print n
+s = 0
+n = 0
+
+sprompt('Input lowercase r')
+s = 0
+
+nprompt('Input lowercase a')
+print n
+print s
+n = 0
+s = 0
+
+nprompt('Input lowercase b')
+print n
+print s
+n = 0
+s = 0
+
+nprompt('Input lowercase c')
+print n
+print s
+n = 0
+s = 0
+
+nprompt('Input lowercase d')
+print n
+print s
+n = 0
+s = 0
+
+nprompt('Input lowercase r')
+n = 0
+s = 0
+
+fprompt('Input lowercase a')
+print f
+print t
+f = 0
+t = 0
+
+fprompt('Input lowercase b')
+print f
+print t
+f = 0
+t = 0
+
+fprompt('Input lowercase c')
+print f
+print t
+f = 0
+t = 0
+
+fprompt('Input lowercase d')
+print f
+print t
+f = 0
+t = 0
+
+fprompt('Input lowercase r')
+f = 0
+t = 0
+
+tprompt('Input lowercase a')
+print t
+print f
+t = 0
+f = 0
+
+tprompt('Input lowercase b')
+print t
+print f
+t = 0
+f = 0
+
+tprompt('Input lowercase c')
+print t
+print f
+t = 0
+f = 0
+
+tprompt('Input lowercase d')
+print t
+print f
+t = 0
+f = 0
+
+tprompt('Input lowercase r')
+t = 0
+f = 0
+
+jprompt('Input lowercase a')
+print j
+print p
+j = 0
+p = 0
+
+jprompt('Input lowercase b')
+print j
+print p
+j = 0
+p = 0
+
+jprompt('Input lowercase c')
+print j
+print p
+j = 0
+p = 0
+
+jprompt('Input lowercase d')
+print j
+print p
+j = 0
+p = 0
+
+jprompt('Input lowercase r')
+j = 0
+p = 0
+
+pprompt('Input lowercase a')
+print p
+print j
+p = 0
+j = 0
+
+pprompt('Input lowercase b')
+print p
+print j
+p = 0
+j = 0
+
+pprompt('Input lowercase c')
+print p
+print j
+p = 0
+j = 0
+
+pprompt('Input lowercase d')
+print p
+print j
+p = 0
+j = 0
+
+pprompt('Input lowercase r')
+p = 0
+j = 0
+
+print('done')
+
+print('Next time I will be debugging the zodiac prompt.')
